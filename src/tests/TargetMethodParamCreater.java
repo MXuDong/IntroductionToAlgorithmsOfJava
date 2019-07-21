04697f5a491583f0525b2c0fc4b0fed1a5f03f99
MXuDong <2941884109@qq.com>
2019-07-21 19:27:09 +0800

    更新注解

--- src/tests/TargetMethodParamCreater.java
+++ src/tests/TargetMethodParamCreater.java
@@ -9,4 +9,10 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TargetMethodParamCreater {
+    /**
+     * 该方法应该被测试的次数
+     *
+     * @return 测试的次数
+     */
+    int methodInvokeCount() default 1;
 }
