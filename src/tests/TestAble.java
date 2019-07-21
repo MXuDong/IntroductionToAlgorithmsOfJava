7ee833ef26d91467357a1a3b66ee67d3f631bfe9
MXuDong <2941884109@qq.com>
2019-07-21 19:20:28 +0800

    添加测试接口

---
+++ src/tests/TestAble.java
@@ -0,0 +1,22 @@
+package tests;
+
+/**
+ * 测试接口
+ */
+public interface TestAble {
+    /**
+     * 获取格式化的参数
+     *
+     * @param objects 目标参数
+     * @return 格式化后的字符串
+     */
+    String getParamFormat(Object... objects);
+
+    /**
+     * 获取格式化后的结果
+     *
+     * @param objects 目标结果
+     * @return 格式化后的肌肤穿
+     */
+    String getResultFormat(Object... objects);
+}
--- src/tests/UtilTest.java
+++ src/tests/UtilTest.java
@@ -1,9 +1,35 @@
 package tests;
 
+import io.github.mxudong.rs.Reflector;
+import io.github.mxudong.rs.packings.methods.Invoker;
+
 /**
  * 测试工具
  *
  * @author Dong
  */
-public interface UtilTest {
+public class UtilTest {
+
+    /**
+     * 需要测试数据集的方法
+     */
+    private Invoker [] targetParamInvokers;
+
+    private long startTime;
+    private long stopTime;
+
+    /**
+     * 执行方法
+     *
+     * @param tClass 目标类
+     * @param targetMethodName 目标方法名
+     */
+    public void start(Class tClass, String targetMethodName){
+
+        long startTime = TimeUtil.getCurrentTime();
+
+        Reflector reflector = new Reflector(tClass);
+        Invoker[] methods = reflector.getClassObject().getMethods();
+
+    }
 }
