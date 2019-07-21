a8032a2493cfdaeb4acbaaac6c558af8d96df223
MXuDong <2941884109@qq.com>
2019-07-21 18:53:13 +0800

    添加方法

--- src/tests/ArrayUtil.java
+++ src/tests/ArrayUtil.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.github.mxudong.rs.randoms.BaseRandom;
+
 /**
  * 数组测试工具
  *
@@ -12,6 +14,7 @@ public class ArrayUtil {
      *
      * @param array            被检查数组
      * @param isAscendingOrder 是否为升序
+     * @return is order
      */
     public boolean isSort(int[] array, boolean isAscendingOrder) {
         if (isAscendingOrder) {
@@ -30,7 +33,19 @@ public class ArrayUtil {
         return true;
     }
 
-    public int [] getRandomArray(int minInteger, int maxInteger, int arrayLength){
-
+    /**
+     * 生成一个随机数组
+     *
+     * @param minInteger  最小值
+     * @param maxInteger  最大值
+     * @param arrayLength 数组长度
+     * @return 随机数组
+     */
+    public int[] getRandomArray(int minInteger, int maxInteger, int arrayLength) {
+        int[] ints = new int[arrayLength];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = BaseRandom.getRandomInt(minInteger, maxInteger);
+        }
+        return ints;
     }
 }
